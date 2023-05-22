2fd842317fca49b094a04fe83cc701e1554161d7
StarNight346 <112832377+StarNight346@users.noreply.github.com>
2023-05-21 22:09:10 -0400

    Update Main.java
    This is the main class with the added Javadoc

--- Main.java
+++ Main.java
@@ -1,4 +1,19 @@
+
+	/**
+     *  This is the main class of the program. This class holds void main method to run the program.
+     *
+	 * @author Adamb
+	 * @version 1.2
+	 * @since 05-21-2023
+	 */
 public class Main {
+    
+    /**
+     * This is the main method method that creates the object and displays the descriptive text.
+     *
+	 * @param args An array of command arguments for the method.
+	 * @return void This method does not return anything.
+	 */
     public static void main(String[] args)
     {
         FibonacciRecursive Fibon = new FibonacciRecursive();
