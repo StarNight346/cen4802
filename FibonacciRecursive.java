1aee1c8b6b8f749695f2d66d01183dc65ad2396f
StarNight346 <112832377+StarNight346@users.noreply.github.com>
2023-05-21 21:38:50 -0400

    Update FibonacciRecursive.java
    Added Javadoc to the FibonacciRecusive class

--- FibonacciRecursive.java
+++ FibonacciRecursive.java
@@ -1,5 +1,18 @@
+/**
+ *This class is used to create an object that can return the total of a Fibonacci Sequence based on the nth term used in the method "Fibonacci"
+ * 
+ * @author Adamb
+ * @version 1.2
+ * @since 05-21-2023
+*/
+
 public class FibonacciRecursive {
 
+        /**This method is used to get the total of the Fibonacci sequence based on the parameter of the nth term.
+	 * 
+         * @param int Takes in the nth term in the Fibonacci sequence. 
+	 * @return This returns a int, the total of all numbers added together at the nth term in the sequence.
+	 */
         public int Fibonacci(int n) {
 
             if (n==0)
